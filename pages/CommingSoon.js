9c32245bc5c1b5a120eb04bd0b52081530e5c997
Satvik Shukla <62174006+deadland2002@users.noreply.github.com>
2023-02-10 01:04:27 +0530

    Events Page

---
+++ pages/CommingSoon.js
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar";
+import Head from "next/head";
+const CommingSoon=()=>{
+    return(
+        <>
+            <Head>
+                
+            </Head>
+            <Navbar/>
+        </>
+    )
+};
+export default CommingSoon;
